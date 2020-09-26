aaa33b6e7f66e341f454348211cfcb7796656e7b
Mg Kaung <mymemory692018@gmail.com>
2020-09-26 22:31:34 +0630

    changes json data for detail

--- app/src/main/java/mk/podcast/com/datas/models/impls/RealtimeDatabaseFirebaseApiImpl.kt
+++ app/src/main/java/mk/podcast/com/datas/models/impls/RealtimeDatabaseFirebaseApiImpl.kt
@@ -3,7 +3,7 @@ package mk.podcast.com.datas.models.impls
 import mk.podcast.com.datas.models.BaseModel
 import mk.podcast.com.datas.models.PodcastModels
 
-
+//
 //object RealtimeDatabaseFirebaseApiImpl : PodcastModels, BaseModel() {
 //
 //
