2a140190816a35247faa63b1e4ad61b4386cf8c5
Mg Kaung <mymemory692018@gmail.com>
2020-09-26 22:17:05 +0630

    add json

--- app/src/main/java/mk/podcast/com/datas/models/impls/RealtimeDatabaseFirebaseApiImpl.kt
+++ app/src/main/java/mk/podcast/com/datas/models/impls/RealtimeDatabaseFirebaseApiImpl.kt
@@ -1,75 +1,10 @@
 package mk.podcast.com.datas.models.impls
 
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
-import io.reactivex.Observable
-import mk.podcast.com.datas.vos.DetailEpisodeVO
-import mk.podcast.com.datas.vos.RandomPodcastVO
-import mk.podcast.com.networks.PodcastApi
-import mk.podcast.com.networks.responses.GetBestPodcastResponse
-import mk.podcast.com.networks.responses.GetGenericsResponse
-import mk.podcast.com.networks.responses.GetPlayListResponse
+import mk.podcast.com.datas.models.BaseModel
+import mk.podcast.com.datas.models.PodcastModels
 
 
-object RealtimeDatabaseFirebaseApiImpl : PodcastApi {
-
-    private val database: DatabaseReference = Firebase.database.reference
-    override fun fetchPlayList(apiKey: String, id: String): Observable<GetPlayListResponse> {
-        TODO("Not yet implemented")
-    }
-
-    override fun fetchDetailEpisodeByID(apiKey: String, id: String): Observable<DetailEpisodeVO> {
-        TODO("Not yet implemented")
-    }
-
-    override fun fetchPodcastGenresList(
-        apiKey: String,
-        top_level_only: Int
-    ): Observable<GetGenericsResponse> {
-        TODO("Not yet implemented")
-    }
-
-    override fun fetchBestPodcastGenresList(
-        apiKey: String,
-        genre_id: Int,
-        page: Int,
-        region: String,
-        safe_mode: Int
-    ): Observable<GetBestPodcastResponse> {
-        TODO("Not yet implemented")
-    }
-
-    override fun fetchRandomPodcastEpisode(apiKey: String): Observable<RandomPodcastVO> {
-        TODO("Not yet implemented")
-    }
-
-//    override fun getGroceries(
-//        onSuccess: (groceries: List<GroceryVO>) -> Unit,
-//        onFialure: (String) -> Unit
-//    ) {
-//        database.child("groceries").addValueEventListener(object : ValueEventListener {
-//            override fun onCancelled(error: DatabaseError) {
-//                onFialure(error.message)
-//            }
+//object RealtimeDatabaseFirebaseApiImpl : PodcastModels, BaseModel() {
 //
-//            override fun onDataChange(snapshot: DataSnapshot) {
-//                val groceryList = arrayListOf<GroceryVO>()
-//                snapshot.children.forEach { dataSnapShot ->
-//                    dataSnapShot.getValue(GroceryVO::class.java)?.let {
-//                        groceryList.add(it)
-//                    }
-//                }
-//                onSuccess(groceryList)
-//            }
-//        })
-//    }
 //
-//    override fun addGrocery(name: String, description: String, amount: Int) {
-//        database.child("groceries").child(name).setValue(GroceryVO(name,description,amount))
-//    }
-//
-//    override fun deleteGrocery(name: String) {
-//        database.child("groceries").child(name).removeValue()
-//    }
-}
+//}
